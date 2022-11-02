8820d8504d0bfae90a40f08a8985c79aa56679bc
Brayan Diaz C <brayandiazc@gmail.com>
2022-11-02 10:26:40 -0500

    [FEAT] Index view completed

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Brayan Diaz C';
-export const SITE_DESCRIPTION = 'Full Stack Developer | Coding | Teacher | Blogging 😁!';
+export const SITE_DESCRIPTION = 'Full Stack Developer | Coding | Teacher | Blogging!';
